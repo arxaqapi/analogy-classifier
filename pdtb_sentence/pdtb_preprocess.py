fdad79cbff4da07ca782fb06073e73cbc01a0c80
arxaqapi <arxaqapi@protonmail.com>
2021-04-20 23:09:11 +0200

    feat: replaced semantic_split with L1 L2 L3 split

--- pdtb_sentence/pdtb_preprocess.py
+++ pdtb_sentence/pdtb_preprocess.py
@@ -3,8 +3,10 @@ import os
 import glob
 import csv
 
+
 def extract_columns_to_csv(data, columns, name="extract.csv", mode='a'):
     data.to_csv(name, sep='|', columns=columns, index=False, mode=mode)
+
 
 def create_single_csv_from_pdtb(path="pdtb_sentences.csv", columns=[0, 24, 34]):
     """Creates a single .csv containing all pair sentences in the pdtb database
@@ -28,6 +30,7 @@ def create_single_csv_from_pdtb(path="pdtb_sentences.csv", columns=[0, 24, 34]):
         # extract colmns to csv
         extract_columns_to_csv(data, columns=columns, name=path, mode='a')
 
+
 def split_single_csv_into_relation_type_files(path):
     data_dict = {}
     with open(path, 'r') as f:
@@ -49,9 +52,15 @@ def split_single_csv_into_relation_type_files(path):
                 csv_file.writerow(row)
     return data_dict
 
-def split_single_csv_into_semantic_relation_files(path):
+
+def split_single_csv_into_L1_relations(path, single_file=False):
     data_dict = {}
-    prefix = "pdtb_semantic_split/"
+    prefix = "pdtb_L1_split/"
+    if not os.path.exists(prefix):
+        os.mkdir(prefix)
+    else:
+        os.system('rm -rf ' + prefix)
+        os.mkdir(prefix)
     with open(path, 'r') as f:
         csv_file = csv.reader(f, delimiter='|')
         for row in csv_file:
@@ -59,24 +68,32 @@ def split_single_csv_into_semantic_relation_files(path):
                 relationA = row[0].split(".")[0]
                 if relationA not in data_dict.keys():
                     data_dict[relationA] = []
-                else:
-                    data_dict[relationA].append(row[2:])
-                    
+                # add to dict
+                data_dict[relationA].append(row[2:])
+
                 relationB = row[1].split(".")[0]
                 if relationB not in data_dict.keys():
                     data_dict[relationB] = []
-                else:
-                    data_dict[relationB].append(row[2:])
-    if os.path.exists(prefix + "all_sentences.csv"):
-        os.remove(prefix + "all_sentences.csv")
-    for section in data_dict.keys():
-        if section == '':
-            continue
-        path_to_file = prefix + "pdtb" + str(section) + ".pipe.csv"
+                # add to dict
+                data_dict[relationB].append(row[2:])
+    if single_file:
+        path_to_file = prefix + "pdtb_L1_relation.pipe.csv"
         with open(path_to_file, 'w') as f:
             csv_file = csv.writer(f, delimiter='|')
-            for row in data_dict[section]:
-                csv_file.writerow(row)
+            for section in data_dict.keys():
+                if section == '':
+                    continue
+                for row in data_dict[section]:
+                    csv_file.writerow([section] + row)
+    else:
+        for section in data_dict.keys():
+            if section == '':
+                continue
+            path_to_file = prefix + "pdtb" + str(section) + ".pipe.csv"
+            with open(path_to_file, 'w') as f:
+                csv_file = csv.writer(f, delimiter='|')
+                for row in data_dict[section]:
+                    csv_file.writerow(row)
     return data_dict
 
 
@@ -86,45 +103,34 @@ def split_single_csv_into_L2_relations(path, single_file='False'):
     if not os.path.exists(prefix):
         os.mkdir(prefix)
     else:
-        # os.remove(prefix)
         os.system('rm -rf ' + prefix)
         os.mkdir(prefix)
     # read big file
-    lines = 0
-    skipping = 0
     with open(path, 'r') as f:
         csv_file = csv.reader(f, delimiter='|')
         for row in csv_file:
             # skip dead lines
             if row != ['11', '12', '24', '34']:
                 relations_A = row[0].split('.')
-                # if only L1 or len() == 0 SKIP
+                # Skip if not L2
                 if len(relations_A) < 2:
-                    # skip
-                    skipping += 1
-                    continue 
+                    continue
                 L2_relation_A = relations_A[0] + '.' + relations_A[1]
                 # add to dict rel_A
                 if L2_relation_A not in data_dict.keys():
                     data_dict[L2_relation_A] = []
-                else:
-                    lines += 1
-                    data_dict[L2_relation_A].append(row[2:])
+                data_dict[L2_relation_A].append(row[2:])
 
                 # relations B
-                relations_A = row[1].split('.')
-                # if only L1 or len() == 0 SKIP
-                if len(relations_A) < 2:
-                    # skip
-                    skipping += 1
-                    continue 
-                L2_relation_A = relations_A[0] + '.' + relations_A[1]
-                # add to dict rel_A
-                if L2_relation_A not in data_dict.keys():
-                    data_dict[L2_relation_A] = []
-                else:
-                    lines += 1
-                    data_dict[L2_relation_A].append(row[2:])
+                relations_B = row[1].split('.')
+                # Skip if not L2
+                if len(relations_B) < 2:
+                    continue
+                L2_relation_B = relations_B[0] + '.' + relations_B[1]
+                # add to dict rel_B
+                if L2_relation_B not in data_dict.keys():
+                    data_dict[L2_relation_B] = []
+                data_dict[L2_relation_B].append(row[2:])
     if single_file:
         path_to_file = prefix + "pdtb_L2_relation.pipe.csv"
         with open(path_to_file, 'w') as f:
@@ -144,7 +150,66 @@ def split_single_csv_into_L2_relations(path, single_file='False'):
                 csv_file = csv.writer(f, delimiter='|')
                 for row in data_dict[section]:
                     csv_file.writerow(row)
-    print(f"{lines=} | {skipping=}")
     return data_dict
 
+
+def split_single_csv_into_L3_relations(path, single_file='False'):
+    data_dict = {}
+    prefix = "pdtb_L3_split/"
+    if not os.path.exists(prefix):
+        os.mkdir(prefix)
+    else:
+        os.system('rm -rf ' + prefix)
+        os.mkdir(prefix)
+    # read big file
+    with open(path, 'r') as f:
+        csv_file = csv.reader(f, delimiter='|')
+        for row in csv_file:
+            # skip dead lines
+            if row != ['11', '12', '24', '34']:
+                relations_A = row[0].split('.')
+                # Skip if not L3
+                if len(relations_A) < 3:
+                    continue
+                L2_relation_A = relations_A[0] + '.' + \
+                    relations_A[1] + '.' + relations_A[2]
+                # add to dict rel_A
+                if L2_relation_A not in data_dict.keys():
+                    data_dict[L2_relation_A] = []
+                data_dict[L2_relation_A].append(row[2:])
+
+                # relations B
+                relations_B = row[1].split('.')
+                # Skip if not L3
+                if len(relations_B) < 3:
+                    continue
+                L2_relation_B = relations_B[0] + '.' + relations_B[1] + '.' + relations_B[2]
+                # add to dict rel_B
+                if L2_relation_B not in data_dict.keys():
+                    data_dict[L2_relation_B] = []
+                data_dict[L2_relation_B].append(row[2:])
+    if single_file:
+        path_to_file = prefix + "pdtb_L3_relation.pipe.csv"
+        with open(path_to_file, 'w') as f:
+            csv_file = csv.writer(f, delimiter='|')
+            for section in data_dict.keys():
+                if section == '':
+                    continue
+                for row in data_dict[section]:
+                    csv_file.writerow([section] + row)
+    else:
+        for section in data_dict.keys():
+            if section == '':
+                continue
+                # create single files
+            path_to_file = prefix + "pdtb" + str(section) + ".pipe.csv"
+            with open(path_to_file, 'w') as f:
+                csv_file = csv.writer(f, delimiter='|')
+                for row in data_dict[section]:
+                    csv_file.writerow(row)
+    return data_dict
+
+
+# split_single_csv_into_L1_relations("pdtb/semantic_sentence_database.csv", single_file=True)
 # split_single_csv_into_L2_relations("pdtb/semantic_sentence_database.csv", single_file=True)
+# split_single_csv_into_L3_relations("pdtb/semantic_sentence_database.csv", single_file=True)
