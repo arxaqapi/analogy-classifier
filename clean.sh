178d84862c62b03e24e30d665ed048c4aa444d5b
arxaqapi <arxaqapi@protonmail.com>
2021-04-20 23:02:44 +0200

    fix: renaming folders

--- clean.sh
+++ clean.sh
@@ -12,7 +12,7 @@ rm -rf pdtb_sentence/*.csv
 rm -rf pdtb_sentence/temp/
 rm -rf pdtb_sentence/pdtb/*.csv
 rm -rf pdtb_sentence/pdtb_split/
-rm -rf pdtb_sentence/pdtb_semantic_split/
+rm -rf pdtb_sentence/pdtb_L1_split/
 rm -rf pdtb_sentence/__pycache__/
 
 rm -rf sentence_forest/__pycache__/
--- pdtb_sentence/clean.sh
+++ pdtb_sentence/clean.sh
@@ -1,4 +1,4 @@
 rm *.csv
 rm pdtb/*.csv
 rm pdtb_split/*.csv
-rm pdtb_semantic_split/*.csv
+rm pdtb_L1_split/*.csv
